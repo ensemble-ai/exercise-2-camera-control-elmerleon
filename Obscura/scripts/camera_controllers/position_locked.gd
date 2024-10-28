574be88e29d66a09a0f088e9c267fc7a0d458bf1
elmerleon <54426335+elmerleon@users.noreply.github.com>
2024-10-27 18:58:26 -0700

    stage 1
    added position lock and modified the drawing of the box to create a cross

--- Obscura/scripts/camera_controllers/position_locked.gd
+++ Obscura/scripts/camera_controllers/position_locked.gd
@@ -1,6 +1,5 @@
-class_name PushBox
+class_name PositionLock
 extends CameraControllerBase
-
 
 @export var box_width:float = 10.0
 @export var box_height:float = 10.0
@@ -23,24 +22,9 @@ func _process(delta: float) -> void:
 	
 	#boundary checks
 	#left
-	var diff_between_left_edges = (tpos.x - target.WIDTH / 2.0) - (cpos.x - box_width / 2.0)
-	if diff_between_left_edges < 0:
-		global_position.x += diff_between_left_edges
-	#right
-	var diff_between_right_edges = (tpos.x + target.WIDTH / 2.0) - (cpos.x + box_width / 2.0)
-	if diff_between_right_edges > 0:
-		global_position.x += diff_between_right_edges
-	#top
-	var diff_between_top_edges = (tpos.z - target.HEIGHT / 2.0) - (cpos.z - box_height / 2.0)
-	if diff_between_top_edges < 0:
-		global_position.z += diff_between_top_edges
-	#bottom
-	var diff_between_bottom_edges = (tpos.z + target.HEIGHT / 2.0) - (cpos.z + box_height / 2.0)
-	if diff_between_bottom_edges > 0:
-		global_position.z += diff_between_bottom_edges
+	global_position = target.global_position
 		
 	super(delta)
-
 
 func draw_logic() -> void:
 	var mesh_instance := MeshInstance3D.new()
@@ -56,17 +40,13 @@ func draw_logic() -> void:
 	var bottom:float = box_height / 2
 	
 	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(right-5, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right-5, 0, bottom))
 	
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom-5))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom-5))
 	
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
 	
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
 	
 	immediate_mesh.surface_end()
 
